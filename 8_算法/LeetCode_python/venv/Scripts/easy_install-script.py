d2f0505ee592f5773575508472fae9a4d4140b13
huster_CAD <37055974+xyk1994@users.noreply.github.com>
2019-03-11 15:03:38 +0800

    add exercise file

---
+++ 8_算法/LeetCode_python/venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!C:\File\Algorithm-Interview-And-Written-Summary\8_Ëã·¨\LeetCode_python\venv\Scripts\python.exe -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
+__requires__ = 'setuptools==39.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.1.0', 'console_scripts', 'easy_install')()
+    )
